7c0d70a8e3e3f43a05571022a99f1f4806d57b57
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-05-19 16:07:12 +0200

    links favourite pages items to detail page

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,5 +1,66 @@
-const Favourites = () => {
-  return <div>❤️</div>;
+import { Col, Container, Row, ListGroup, Button, Badge } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { removeFavourite } from 'redux/actions';
+import { useHistory } from 'react-router-dom';
+
+const mapStatetoProps = (state) => ({
+  favourites: state.favourites,
+});
+
+const Favourites = ({ favourites, removeFavourite }) => {
+  const history = useHistory();
+  const handleRemoveFav = (id) => {
+    removeFavourite(id);
+  };
+
+  return (
+    <Container className='min-vh-100 mt-3'>
+      <Row className='justify-content-center'>
+        <Col>
+          {favourites.length === 0 && (
+            <div className='text-center'>
+              <h1 className='text-center text-white'>
+                No fav yet, go add some!
+              </h1>
+              <Button
+                variant='warning'
+                onClick={() => history.push('/')}
+                className='rounded-circle mt-3'
+              >
+                {' '}
+                🏠{' '}
+              </Button>
+            </div>
+          )}
+          <ListGroup>
+            {favourites.map((fav) => (
+              <ListGroup.Item key={fav.id} className='d-flex lh-lg'>
+                <span className='me-auto'>
+                  {fav.title} - {fav.company}
+                </span>
+
+                <span
+                  onClick={() => handleRemoveFav(fav.id)}
+                  style={{ cursor: 'pointer' }}
+                  className='me-3'
+                >
+                  💔
+                </span>
+
+                <Badge
+                  onClick={() => history.push(`/details/${fav.id}`)}
+                  style={{ cursor: 'pointer' }}
+                  className='bg-secondary lh-lg'
+                >
+                  Details
+                </Badge>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
-export default Favourites;
+export default connect(mapStatetoProps, { removeFavourite })(Favourites);
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,4 +1,4 @@
-import { Button, Col } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { format } from 'date-fns';
 import { connect } from 'react-redux';
